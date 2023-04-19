6a8063b554be6ad82d72aab78d5a9803c7ab2d43
Rohan <65642059+rohansahni@users.noreply.github.com>
2023-04-19 20:33:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,9 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+from streamlit_webrtc import webrtc_streamer
 
+webrtc_streamer(key="sample")
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
