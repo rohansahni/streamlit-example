8fd14e6eefce8317a4b4a1a11b6b5d1b6ed45b34
Rohan <65642059+rohansahni@users.noreply.github.com>
2023-04-21 13:25:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,26 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+
 import streamlit as st
+import av
+import aiortc
+from aiortc.contrib.media import MediaPlayer
 
-"""
-# Welcome to Streamlit!
+def main():
+    st.title("WebRTC Demo")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+    # Create a WebRTC peer connection
+    pc = aiortc.RTCPeerConnection()
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    # Add a video track
+    video = st.video()
+    video_track = pc.addTrack(aiortc.VideoStreamTrack())
+    @video.on_close
+    def on_close():
+        pc.removeTrack(video_track)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    # Add an audio track
+    audio_track = pc.addTrack(aiortc.AudioStreamTrack())
+    player = MediaPlayer(audio_track)
+    player.play()
 
-from streamlit_webrtc import webrtc_streamer
-
-webrtc_streamer(key="sample")
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == "__main__":
+    main()
